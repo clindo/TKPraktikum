2f02473cb1f4f3f1b35ae8156cc7caa389b673d4
sachinmanawadi271 <sachin.manawadi@gmail.com>
2017-04-04 22:21:39 +0200

    Reverting exception handling

--- PythonScripts/BussinessLogic.py
+++ PythonScripts/BussinessLogic.py
@@ -13,62 +13,47 @@ class business_logic:
         backslash = "\\"
         app_path = _config.Original_Path+backslash+file
         #print(app_path)
-        try:
-            os.startfile(app_path)
-        except OSError as e:
-            print "Path not found : Enter a valid path in configuration.xml "
+        os.startfile(app_path)
         time.sleep(_config.App_time)
         print("Sleep End!!!")
-        try:
-            app = Application().connect(path=r"C:\Program Files\TechSmith\Camtasia 9\CamtasiaStudio.exe")
-        except pywinauto.application.AppNotConnected, pywinauto.application.AppStartError:
-            print "Application not connected!!"
+        app = Application().connect(path=r"C:\Program Files\TechSmith\Camtasia 9\CamtasiaStudio.exe")
 
         #for TRIAL
         if _config.Trial == 'YES':
             #app.Window_(best_match='Dialog', top_level_only=True).ChildWindow(best_match='Finish').SetFocus()
-            try:
-                app.Window_(best_match='Dialog', top_level_only=True).ChildWindow(best_match='Finish').Click()
-            except pywinauto.findwindows.WindowNotFoundError:
-                print "Window not found!!"
+            app.Window_(best_match='Dialog', top_level_only=True).ChildWindow(best_match='Finish').Click()
+            time.sleep(3)
         #end for TRIAL
         child_elements = app[u'Camtasia 9']
         #remark
         child_elements.ClickInput(coords=(_config.Share_Btn_X, _config.Share_Btn_Y))
         child_elements.TypeKeys("{DOWN}")
         child_elements.TypeKeys("{ENTER}")
-        #time.sleep(10)
+        time.sleep(5)
 
         #for TRIAL
         if _config.Trial == 'YES':
             child_elements.Wait('visible',timeout=20)
             child_elements.ClickInput(coords=(_config.Water_Mark_Btn_X, _config.Water_Mark_Btn_Y))
+            time.sleep(5)
         #end for TRIAL
         for i in range(_config.Dialogs):
         #for no_dialogs in _config.Dialogs:
-            #time.sleep(2)
+            time.sleep(2)
             child_elements.Wait('visible',timeout=20)
-            try:
-                app.Window_(best_match='Dialog', top_level_only=True).ChildWindow(best_match='Next').Click()
-            except pywinauto.findwindows.WindowNotFoundError:
-                print "Window not found!!"
+            app.Window_(best_match='Dialog', top_level_only=True).ChildWindow(best_match='Next').Click()
+
         #app.Window_(best_match='Dialog', top_level_only=True).PrintControlIdentifiers()
         #app.Window_(best_match='Dialog', top_level_only=True).ChildWindow(title="Untitled Project",class_name="Edit").SetText(time.time())
         #app.Window_(best_match='Dialog', top_level_only=True).ChildWindow(title="Untitled Project",class_name="Edit").SetText(file)
         stripped_file_name = os.path.splitext(os.path.basename(file))[0]
-        try:
-            app.Window_(best_match='Dialog', top_level_only=True).ChildWindow(title="Untitled Project",class_name="Edit").SetText(stripped_file_name)
-        except pywinauto.findwindows.WindowNotFoundError:
-            print "Window not found!!"
+        app.Window_(best_match='Dialog', top_level_only=True).ChildWindow(title="Untitled Project",class_name="Edit").SetText(stripped_file_name)
         #app.Window_(best_match='Dialog', top_level_only=True).ChildWindow(title="C:\\Users\\sachinbm\\Documents\\Camtasia Studio",class_name="Edit").SetText("E:\\testcamtasia")
-        try:
-            app.Window_(best_match='Dialog', top_level_only=True).ChildWindow(best_match='Finish').Click()
-        except pywinauto.findwindows.WindowNotFoundError:
-            print "Window not found!!"
-        time.sleep(2)
+        app.Window_(best_match='Dialog', top_level_only=True).ChildWindow(best_match='Finish').Click()
+        time.sleep(3)
         #child_elements.Wait('visible',timeout=20)
         app.kill_()
-        time.sleep(10)
+        time.sleep(5)
         #print("Rendering Succcessful")
         return stripped_file_name
         #Finish rendering
--- PythonScripts/app.py
+++ PythonScripts/app.py
@@ -5,43 +5,27 @@ from logger import log
 import sys
 import os, time
 
+
 log = log()
 Auto = Automate()
 log.logger.info('Automate initialized')
 
-try:
-    before = dict([(f, None) for f in os.listdir(Auto.__configuration__.Original_Path)])
-except OSError:
-    log.logger.info("Path not found : Enter a valid path in configuration.xml")
-
-
+before = dict([(f, None) for f in os.listdir(Auto.__configuration__.Original_Path)])
 while 1:
-            print "Inside while loop"
-            time.sleep(5)
-            try:
-              after = dict([(f, None) for f in os.listdir(Auto.__configuration__.Original_Path)])
-            except OSError:
-                log.logger.info("Path not found : Enter a valid path in configuration.xml")
+    time.sleep (10)
+    after = dict ([(f, None) for f in os.listdir (Auto.__configuration__.Original_Path)])
+    added = [f for f in after if not f in before]
 
-            # try:
-            added = [f for f in after if not f in before]
-            # except OSError:
-            #     log.logger.info("Path not found : Enter a valid path in configuration.xml")
+    if added:
+        print "Added: ", ", ".join (added)
+        bi = business_logic()
+        for file in added:
+            file_name = bi.convert_files(file)
+            print "Rendering Succesfull!!"
+            print "Start renaming to index.html"
+            backslash = "\\"
+            x = Auto.__configuration__.Saved_Path+backslash+file_name
+            y = file_name+".html"
+            Auto.renameFile(x,y)
 
-
-            if added:
-                    print "Added: ", ", ".join(added)
-                    bi = business_logic()
-            for file in added:
-                    try:
-                        file_name = bi.convert_files(file)
-                    except OSError:
-                        log.logger.info("Path not found : Enter a valid path in configuration.xml")
-                    print "Rendering Succesfull!!"
-                    print "Start renaming to index.html"
-                    backslash = "\\"
-                    x = Auto.__configuration__.Saved_Path + backslash + file_name
-                    y = file_name + ".html"
-                    Auto.renameFile(x, y)
-
-            before = after
+    before = after
