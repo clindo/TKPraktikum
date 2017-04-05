d93e23bc19f5d4e191cf11466368daee330e5581
Krishnendhu <krishnendhur@gmail.com>
2017-04-05 21:57:24 +0200

    Modified code for checking not rendered files.

--- PythonScripts/Camtasia_UIAUtomation.py
+++ PythonScripts/Camtasia_UIAUtomation.py
@@ -17,15 +17,27 @@ if os.path.exists(Auto.__configuration__.Original_Path):
     log.logger.info("Already existing files:")
     log.logger.info(before)
 else:
-    log.logger.info("Trek file path is invalid.")
+    log.logger.info("Trec file path is invalid.")
     sys.exit()
 while 1:
+
+    fileNames = []
+    with open("SuccessfullyRenderedFiles.txt") as file:
+        for line in file:
+            line = line.strip() #or someother preprocessing
+            fileNames.append(line)
+    differenceList= list(set(before.keys())-set(fileNames))
+    differenceJSON =dict([(f, None) for f in differenceList])
+
+
     #Check new added files after every 10 seconds
     time.sleep (Auto.__configuration__.Polling_time)
     #Get the new added trek files
     if os.path.exists(Auto.__configuration__.Original_Path):
         after = dict ([(f, None) for f in os.listdir (Auto.__configuration__.Original_Path)])
         added = [f for f in after if not f in before]
+        added+=differenceJSON
+        # print  " addedJSON"+added
     else:
         log.logger.info("Trek file path does not exists!")
         sys.exit()
@@ -39,20 +51,24 @@ while 1:
             rendered_file_name = bi.convert_files(file)
             if rendered_file_name :
                 log.logger.info('Rendering Successful')
+                backslash = "\\"
+                full_path_of_rendered_file = Auto.__configuration__.Saved_Path+backslash+rendered_file_name
+                rendered_html_file = rendered_file_name+".html"
+
+                if os.path.exists(full_path_of_rendered_file):
+                    status = Auto.renameFile(full_path_of_rendered_file,rendered_html_file)
+                    if status == 1:
+                        renderedFileList= open("SuccessfullyRenderedFiles.txt","a+")
+                        print rendered_file_name
+                        renderedFileList.write("%s.trec\n" % rendered_file_name)
+                        renderedFileList.close()
+                        log.logger.info("Renaming Successful!")
+                    else:
+                        log.logger.info("Renaming not Successful!")
+                else:
+                    log.logger.info("Rendered file does not exists!")
+                    sys.exit()
             else:
                 log.logger.info('Rendering not successful')
-            backslash = "\\"
-            full_path_of_rendered_file = Auto.__configuration__.Saved_Path+backslash+rendered_file_name
-            rendered_html_file = rendered_file_name+".html"
-
-            if os.path.exists(full_path_of_rendered_file):
-                status = Auto.renameFile(full_path_of_rendered_file,rendered_html_file)
-                if status == 1:
-                    log.logger.info("Renaming Successful!")
-                else:
-                    log.logger.info("Renaming not Successful!")
-            else:
-                log.logger.info("Rendered file does not exists!")
-                sys.exit()
 
     before = after
